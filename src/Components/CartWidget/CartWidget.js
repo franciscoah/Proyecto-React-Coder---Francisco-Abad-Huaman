062e91fffbac1893cfbec71f8d069f15182e7aab
francisco <franciscoah2001@gmail.com>
2024-03-04 18:30:38 -0500

    Entregable 1

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from '../../icons/icon_shopping_cart.svg';
+const CartWidget = () => {
+    return(
+        <div>
+            <img src ={cart} alt = 'cart-widget'></img>
+            0
+        </div>
+
+    )
+}
+
+export default CartWidget;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,38 @@
+import CartWidget from '../CartWidget/CartWidget';
+import logoBar from '../../logos/logo_yard_sale.svg';
+
+const NavBar = () => {
+  return (
+    <nav>
+      
+      <div className="navbar-left">
+      <img src={logoBar} alt="logoBar" className="logo"></img>
+        <ul>
+          <li>
+            <button type="button">All</button>
+          </li>
+          <li>
+            <button type="button">Clothes</button>
+          </li>
+          <li>
+            <button type="button">Electronics</button>
+          </li>
+          <li>
+            <button type="button">Fornitures</button>
+          </li>
+          <li>
+            <button type="button">Collectibles</button>
+          </li>
+          <li>
+            <button type="button">Others</button>
+          </li>
+        </ul>
+      </div>
+
+      <CartWidget/>
+      
+    </nav>
+  );
+};
+
+export default NavBar;
